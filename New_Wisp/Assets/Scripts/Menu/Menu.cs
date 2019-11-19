46d5444e11238239ad7ee14d2f49d8215b2d3a88
PizzaByTheGuy <56232234+PizzaByTheGuy@users.noreply.github.com>
2019-11-19 14:31:24 -0500

    Menu
    Menu added

---
+++ New_Wisp/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HowButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
+
